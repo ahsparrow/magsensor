2d076f7fc2f473678c6401349ea11e7b74319cbf
Alan Sparrow <git@freeflight.org.uk>
2024-07-08 12:17:17 +0100

    Print DING messages

--- echo.py
+++ echo.py
@@ -1,11 +1,11 @@
-import asyncio
 from machine import SPI, Pin
 from mcp2515 import MCP2515 as CAN
 from mcp2515.canio import Message
 import msgid
+import struct
 
 
-async def can_task():
+def can_task():
     spi = SPI(0, sck=Pin(2), mosi=Pin(3), miso=Pin(4))
     cs = Pin(9, Pin.OUT, value=1)
 
@@ -19,17 +19,11 @@ async def can_task():
         if listener.in_waiting():
             rx_msg = listener.receive()
             if rx_msg.id == msgid.ACK:
-                print(f"Received ACK: {rx_msg.data[0]}, {bytes(rx_msg.data)}")
-
-        await asyncio.sleep_ms(0)
-
-
-async def main():
-    try:
-        await asyncio.wait_for(can_task(), timeout=1.0)
-    except asyncio.TimeoutError:
-        pass
+                print(f"ACK: bell {rx_msg.data[0]}, {bytes(rx_msg.data)}")
+            elif rx_msg.id < 16:
+                delay = struct.unpack("<H", rx_msg.data)[0]
+                print(f"DING: bell {rx_msg.id}, delay {delay} , {bytes(rx_msg.data)}")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    can_task()
