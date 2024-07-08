4e86cd719bce8e0cffff2c8fd4db5cd5e7ec1e77
Alan Sparrow <git@freeflight.org.uk>
2024-07-08 12:30:07 +0100

    Set no filtering in receive.py

--- receive.py
+++ receive.py
@@ -5,6 +5,10 @@ from mcp2515 import MCP2515 as CAN
 
 BELLS = "x1234567890ET"
 
+# Accept all messages
+MASKS = [0x0, 0x0]
+FILTERS = [0x0, 0x0, 0x0, 0x0, 0x0, 0x0]
+
 
 # Output the bell message after a specified delay
 async def delay(bell, delay_ms):
@@ -23,6 +27,8 @@ async def main():
     cs = Pin(9, Pin.OUT, value=1)
 
     can = CAN(spi, cs)
+    can.load_filters(MASKS, FILTERS)
+
     listener = can.listen()
 
     while True:
